af11a87247388e888963daada6d81acb34cb976f
Rodrigo Paredes Philippe <rodrigo.paredesp@userena.cl>
2023-10-09 00:04:27 +0000

    Cambio a puerto 5000

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('CIDERE backend');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
